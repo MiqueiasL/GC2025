a185363983202e05ae94c9cfff9ba21158245703
MiqueiasL <miqueiasmlucas@gmail.com>
2025-11-04 15:38:07 -0300

    Estrutura Inicial Projeto

---
+++ java/gc-java-2025-spring/src/main/java/gc_java_2025_spring/gc_java_2025_spring/GcJava2025SpringApplication.java
@@ -0,0 +1,13 @@
+package gc_java_2025_spring.gc_java_2025_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcJava2025SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcJava2025SpringApplication.class, args);
+	}
+
+}
---
+++ java/gc-java-2025-spring/src/test/java/gc_java_2025_spring/gc_java_2025_spring/GcJava2025SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package gc_java_2025_spring.gc_java_2025_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcJava2025SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
